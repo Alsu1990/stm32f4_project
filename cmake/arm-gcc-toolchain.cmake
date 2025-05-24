553925c02704f22202a2f40365419299974d6dc3
Alsu1990 <alexsukmanov1990@gmail.com>
2025-05-24 11:44:03 +0300

    update cmake, add CMSIS_v6 (ARM)

--- cmake/arm-gcc-toolchain.cmake
+++ cmake/arm-gcc-toolchain.cmake
@@ -6,9 +6,9 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(CMAKE_ASM_COMPILER arm-none-eabi-as)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_SIZE arm-none-eabi-size)
 
-# Specify the path to the STM32 headers and libraries
-set(CMAKE_FIND_ROOT_PATH /path/to/STM32/headers /path/to/STM32/libraries)
 
 # Search for programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
