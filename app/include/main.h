8632ab72b8ea4d8fd06a4c0b9f10026fa9b5a03a
Alsu1990 <alexsukmanov1990@gmail.com>
2025-05-24 11:44:41 +0300

    add cortexm4 device template

--- app/include/main.h
+++ app/include/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "stm32f4xx.h" // Adjust this include path based on your STM32 series
+// #include "stm32f4xx.h" // Adjust this include path based on your STM32 series
+#include "core_cm4.h"
 
 void SystemInit(void);
 
